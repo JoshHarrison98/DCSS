a5c0b2e44c506f7691f181b3179863140e10cba6
Gary Allen <g.allen@hud.ac.uk>
2018-06-27 15:47:18 +0100

    Initial Commit.  Code originally written a few years ago

---
+++ README.md
@@ -0,0 +1,6 @@
+A JavaSpaces Print Queue Demo
+
+Gary Allen, University of Huddersfield.
+
+Animates a print queue system using a JavaSpace as a central repository of objects.  Illustrates how to create a queue of objects.
+
---
+++ src/StartPrintQueue.java
@@ -0,0 +1,37 @@
+import net.jini.core.lease.Lease;
+import net.jini.space.JavaSpace;
+
+public class StartPrintQueue{
+    private static final long ONESECOND = 1000;  // one thousand milliseconds
+
+    public static void main(String args[]){
+
+        JavaSpace space = SpaceUtils.getSpace();
+
+        if (space == null){
+            System.err.println("Failed to find the javaspace");
+            System.exit(1);
+        }
+
+        QueueStatus template = new QueueStatus();
+        try {
+            QueueStatus returnedObject = (QueueStatus)space.readIfExists(template,null, ONESECOND);
+            if (returnedObject == null) {
+                // there is no object in the space, so create one
+                try {
+                    QueueStatus qs = new QueueStatus(0);
+                    space.write(qs, null, Lease.FOREVER);
+                    System.out.println("QueueStatus object added to space");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // there is already an object available, so don't create one
+                System.out.println("QueueStatus object is already in the space");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
